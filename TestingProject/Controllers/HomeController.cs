de2e4b732a140ff4d2ebb3d1a190163bb9a2e66c
Thabo Shabalala <102803954+Tbosba@users.noreply.github.com>
2022-07-30 04:21:25 +0200

    Add project files.

---
+++ TestingProject/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestingProject.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+
+        
+        private static string ApiKey = "AIzaSyDIEtRLmFHKfgq6bnmuWtDOtVHb99lmWto";
+        private static string AuthEmail = "";
+        private static string AuthPassword = "";
+        private static string Bucket = "gs://asp-mvc-with-android-dd994.appspot.com";
+
+
+        public ActionResult Index1()
+        {
+            return View();
+        }
+
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
